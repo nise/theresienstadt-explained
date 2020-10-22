9d2e85f03803d044671e86b0883538d7527d36af
friedrich <konstantfriedrich@gmail.com>
2020-10-22 21:35:33 +0200

    1.0
    Former-commit-id: 72aa93e289841a370ce828d8690c405fc2b17a46

---
+++ client/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    rules: {
+        'no-console': 'off',
+        'no-unused-vars': 'off',
+        'max-len': 'off',
+        'no-trailing-spaces': 'off',
+        'no-tabs': 'off',
+        'no-return-assign': 'off',
+        'camelcase': 'off',
+        'promise/always-return': 'off',
+        'promise/catch-or-return': 'off'
+    }
+};
