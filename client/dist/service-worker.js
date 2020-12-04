3722b2a4c664743a4ed1cf9f1814709fb3f3d0e4
friedrich <konstantfriedrich@gmail.com>
2020-12-04 16:40:59 +0100

    main functionality and style implemented
    Former-commit-id: dc9a73a705535e83417d47f2f752875e1cf0273e

--- client/dist/service-worker.js
+++ client/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44d81b1d5f7dfa93eada354bb2e13201.js"
+  "/precache-manifest.ab4fb1b9925b85517c21a7ffebdc359a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "client"});
