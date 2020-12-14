90dd2be69f3cf8c3c5819b70d875338b5c6c0168
Niels Seidel <niels.seidel@fernuni-hagen.de>
2020-12-14 15:18:26 +0100

    added header
    Former-commit-id: 0c722580d022a09970f804cf224aa0867aa243d0

--- client/src/views/Privacy.vue
+++ client/src/views/Privacy.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="container">
-    <h1>Datenschutz</h1>
-
-    <div class="page">
+  <div class="container" style="text-align: left">
+    <h1 class="mt-4 mb-1" style="text-align: left">
+    Datenschutzerklärung
+    </h1>
+    <div style="background-color: white" class="page mt-3 mb-5 pt-4 pb-3 px-3">
     </div>    
   </div>
 </template>
