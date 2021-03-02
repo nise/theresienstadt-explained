44c6e1968daccb3318620afbc68cca535ef6a00b
friedrich <konstantfriedrich@gmail.com>
2021-03-02 09:57:31 +0100

    first test
    Former-commit-id: 2bce46e2ad636e841c14cc07dcfc514665c886da

---
+++ client/src/components/VideoMapMarker.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <track
+      src="../assets/videos/mapMarker/mapMarker.vtt"
+      id="mapmarkertrack"
+      @load="setup($event)"
+      @error="setup($event)"
+    />
+    <portal target="leafletmap">
+      <LeafletMap> </LeafletMap>
+    </portal>
+</div>
+</template>
+
+<script>
+
+import leafletmap from "./LeafletMap";
+
+export default {
+  name: "VideoMapMarker",
+  components: {
+    LeafletMap: leafletmap,
+  },
+
+  methods: {
+    setup(event) {
+      //var ttrack = event.target.track;
+      //ttrack.mode = "hidden";
+      console.log(event.target.track);
+    },
+  },
+  mounted: function () {},
+};
+</script>
+
+<style>
+</style>
