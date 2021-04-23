63bf1c05d3174390f720144eabe8f6ce23dffadd
Niels Seidel <niels.seidel@fernuni-hagen.de>
2021-04-23 12:02:58 +0200

    started on issue #38
    Former-commit-id: 6e99b841390a580884bcff4c349dae9864433c4f

--- client/src/views/Privacy.vue
+++ client/src/views/Privacy.vue
@@ -4,15 +4,85 @@
     Datenschutzerklärung
     </h1>
     <div style="background-color: white" class="page mt-3 mb-5 pt-4 pb-3 px-3">
+      <h2>Technische Angaben</h2>
+      <p>...</p>
+
+      <!-- APLE Moodle-->
+      <span class="aple">
+
+      <p>Verantwortlich für das Webangebot und somit auch verantwortlich für die
+          Datenverarbeitungen auf der Plattform ist der Förderverein Jugendbegegnung in Theresienstadt/Terezin, hier vertreten durch die beiden Vortsände:</p>
+        <ul>
+          <li>
+            <p>
+              <b>Armin Pietsch</b>,<br />
+              joerg.haake@fernuni-hagen.de<br /> FernUniversität in Hagen, <br />Lehrgebiet
+              Kooperative Systeme, <br />Universitätsstraße 1, 58084 Hagen</p>
+          </li>
+          <li>
+            <p><b>Dr. Frauke Wenzel</b>,<br />
+              claudia.dewitt@fernuni-hagen.de<br /> FernUniversität in Hagen,<br />
+              Lehrgebiet Bildungstheorie und Medienpädagogik,<br /> Universitätsstraße
+              33, 58084 Hagen</p>
+          </li>
+        </ul>
+      </p>
+      </span>
+      <!-- Ende APLE Moodle-->
+
+      <p>Die Lehr-/Lernplattform Moodle wird in einer sicheren technischen Umgebung auf Servern der
+      FernUniversität in Hagen im Maschinensaal des ZMI auf dem Campus betrieben.
+      Die Systemumgebung beinhaltet jeweils eigens für die Moodle-Instanzen eingerichtete Datenbanken
+      mit den Kurs- und Nutzungsdaten und ebenfalls eigens eingerichteten Web-Servern, auf denen
+      neben dem Programmcode von Moodle auch die von Nutzer*innen hochgeladenen Dateien
+      abgelegt werden. Zugriff auf diese IT-Basis haben ausschließlich die Systemadministrator*innen im
+      ZMI <span class="aple">sowie ein Administrator aus dem Projekt APLE</span>. Für die Nutzenden der Moodle-Instanzen ohne Admin-Rechte sind die Daten nur webbasiert über die Funktionen in Moodle zugänglich. Die Schnittstelle zum LDAP-Verzeichnis für die Authentifizierung beim Login wird von den Systemadministrator*innen im ZMI konfiguriert.</p>
+      <!-- APLE Moodle-->
+      <span class="aple">
+      <p>Verantwortlich für die technische Durchführung und Administration der Plattform:</p>
+        <ul>
+          <li>
+            <p><b>Dr. Niels Seidel</b>,<br />
+              niels.seidel@fernuni-hagen.de<br /> FernUniversität in Hagen,<br /> Lehrgebiet
+              Kooperative Systeme,<br /> Universitätsstraße 1, 58084 Hagen</p>
+          </li>
+        </ul>
+      </p>
+      </span>
+
+
+      <h2>Nutzung der Daten und Datenschutz</h2>
+      <p>...
+      </p>
+
+      <h3>Personenbezogene Daten</h3>
+      <p>...</p>
+
+
+
+
+      <h3>Die Rollen und Rechte</h3>
+      <p></p>
+
+
+
+      <h3>Haftung der FernUniversität</h3>
+      <p>Als Dienstanbieterin ist die FernUniversität in Hagen gemäß §§ 7, Abs.1 und 2 TMG für eigene
+      Informationen verantwortlich, die sie zur Nutzung bereithält. Nach §§ 8 TMG besteht keine
+      Verantwortung für die von Studierenden eingestellten Informationen oder für die von anderen
+      Anbieter*innen bereit gestellten Inhalte, auf die mittels Hyperlinks verwiesen wird.</p>
+
+      <h3>Löschfristen</h3>
+      <p></p>
+
+      <h3>Weitere Informationen</h3>
+      <p>....
+      Technische Hilfestellung bietet der Helpdesk des ZMI, Tel. 02331-987 4444 bzw. E-Mail
+      <a href="mailto:helpdesk@fernuni-hagen.de">helpdesk@fernuni-hagen.de</a>.</p>
+
     </div>    
   </div>
 </template>
 
 <script>
-// Import the editor: https://github.com/scrumpy/tiptap
-import { Editor, EditorContent } from 'tiptap'
-
-export default {
-
-}
 </script>
