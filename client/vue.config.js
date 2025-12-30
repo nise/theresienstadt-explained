219510bbe53db47fd66fd244850009d19cd4ce19
nise <niels.seidel@nise81.com>
2025-12-30 17:34:50 +0100

    quick fix for node version 21+
    Former-commit-id: 706a618eb862f33246e07b522ffc8bb2c9b22644

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,33 +1,33 @@
 //// vtt (and other file types) handling: https://forum.vuejs.org/t/vtt-handling/65657/2
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-  outputDir: 'dist',
+  lintOnSave: false, // Disable ESLint during serve
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  outputDir: "dist",
   css: { extract: false },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     // your config
     config.module
-      .rule('vue')
-      .use('vue-loader')
-      .loader('vue-loader')
-      .tap(options => {
-        options['transformAssetUrls'] = {
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) => {
+        options["transformAssetUrls"] = {
           // [...]
-          'b-embed': ['src', 'poster'],
-          source: 'src',
-          track: 'src',
-        }
+          "b-embed": ["src", "poster"],
+          source: "src",
+          track: "src",
+        };
 
-        return options
-      })
+        return options;
+      });
 
     // adjusting the media rule to include vtt files
     config.module
-      .rule('media')
-      .test(/\.(vtt|mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/)
+      .rule("media")
+      .test(/\.(vtt|mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/);
   },
   devServer: {
-    proxy: 'http://localhost:3000'
-  }
-}
-
+    proxy: "http://localhost:3000",
+  },
+};
