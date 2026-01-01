a270da4e50949e58a2eb53a8207a7bb763840d0a
nise <niels.seidel@nise81.com>
2026-01-01 16:46:26 +0100

    build

--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
-import '@babel/polyfill';
-import 'mutationobserver-shim';
-import Vue from 'vue';
-import './plugins/bootstrap-vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
+import "core-js/stable";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 
-import {i18n} from './i18nsetup';
+import { i18n } from "./i18nsetup";
 
 Vue.config.productionTip = false;
-
 
 new Vue({
   router,
   store,
   i18n,
-  render: h => h(App),
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount("#app");
